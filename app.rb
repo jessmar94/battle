f027ec733045bf282d5884bf183a3fcf50585ba6
Jessica Marais <jessica_marais@hotmail.com>
2019-10-02 15:23:05 +0100

    Push code from pairing with Olly

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require 'sinatra/base'
 # set :session_secret, 'super secret'
 
 class Battle < Sinatra::Base
-
+# Get the form on the home page, as designed in the index.erb file. 
   get '/' do
     erb :index
   end
-
+# Return the names and format as designed in the play.erb file.
   post '/names' do
     @player_1_name = params[:player_1_name]
     @player_2_name = params[:player_2_name]
