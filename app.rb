cd82b939108973ca4c3fb71d025e0670ee477a94
zraso <zaira.rasool@hotmail.co.uk>
2019-10-03 14:43:11 +0100

    Creates player class, tests passing

--- app.rb
+++ app.rb
@@ -10,20 +10,20 @@ class Battle < Sinatra::Base
 
 # Return the names and format as designed in the play.erb file.
   post '/names' do
-    session[$Player.new(name)] = params[:player_1_name]
-    session[$Player.new(name)] = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $Player.new(name)
-    @player_2_name = $Player.new(name)
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $Player.new(name)
-    @player_2_name = $Player.new(name)
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 
