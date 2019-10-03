57ebb2afddb8101e60b9cbe51c7aa34227fba672
Jessica Marais <jessica_marais@hotmail.com>
2019-10-03 15:04:33 +0100

    Create passing feature tests. Model layer in progress

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,22 @@
 require 'player'
 
 describe Player do
-  it 'returns players name' do
-    player_1 = Player.new("Jess")
-    expect(player_1.name).to eq 'Jess'
+  let(:player_1) { Player.new("Jess") }
+  let(:player_2) { Player.new("Olly") }
+
+  describe '#initialize' do
+    it 'returns players name' do
+      expect(player_1.name).to eq 'Jess'
+    end
+
+    it 'sets player HP to 60' do
+      expect(player_1.points).to eq 60
+    end
+  end
+
+  describe '#deduct_points' do
+    it 'reduces player 2 by 10 HP' do
+      
+    end
   end
 end
