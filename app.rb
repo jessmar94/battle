3497ca49b2c6bf0eaa868d0b41542faf402aba83
Jessica Marais <jessica_marais@hotmail.com>
2019-10-03 14:18:09 +0100

    Create Player class and use of global variable but tests still failing

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative 'lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -9,20 +10,20 @@ class Battle < Sinatra::Base
 
 # Return the names and format as designed in the play.erb file.
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    session[$Player.new(name)] = params[:player_1_name]
+    session[$Player.new(name)] = params[:player_2_name]
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $Player.new(name)
+    @player_2_name = $Player.new(name)
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $Player.new(name)
+    @player_2_name = $Player.new(name)
     erb :attack
   end
 
---
+++ spec/player_spec.rb
@@ -0,0 +1,8 @@
+require 'player'
+
+describe Player do
+  it 'returns players name' do
+    player_1 = Player.new("Jess") 
+    expect(player_1.name).to eq 'Jess'
+  end
+end
